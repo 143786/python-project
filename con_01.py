134943b9f8122bcc20928f57c22221675a6b703a
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2025-02-05 15:27:52 +0100

    Add files via upload

---
+++ con_01.py
@@ -0,0 +1,9 @@
+a = 5
+
+if a < 5:
+    print("Less than 5 ")
+else:
+    print("equal or greater than 5 ")
+
+# A conditional is a block of code which performs different actions depending on whether a given conditional evaluates
+# to true of false.
