9be4617f32010a29e4306f14f24082330fd2df23
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2025-03-24 14:29:17 +0100

    Add files via upload

---
+++ Python_Blockchain/Note.py
@@ -0,0 +1,11 @@
+
+  # Function in Python :
+  #   Define Code which is executed Later (and possibly Multiple Times ) 
+  #   How to write the function in python : 
+  #   def greet():
+  #     print('Hello')
+  
+  
+  # Calling the function : 
+  # greet()
+  
